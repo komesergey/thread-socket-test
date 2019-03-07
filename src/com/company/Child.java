853d98bc6ba501d6b2ad2b85630a7d1636e30f26
komesergey <komesergey@gmail.com>
2019-03-07 22:22:25 +0300

    Initial commit

---
+++ src/com/company/Child.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Child {
+
+    private static final AtomicInteger ports = new AtomicInteger(30_000);
+
+    private static int servicePort;
+
+    private static int count;
+
+    public static void main(String... args){
+
+        for(String arg: args){
+            if(arg.contains("servicePort")){
+                servicePort = Integer.valueOf(arg.split(":")[1].trim());
+            }
+            if(arg.contains("startPort")){
+                ports.set(Integer.valueOf(arg.split(":")[1].trim()));
+            }
+            if(arg.contains("count")){
+                count = Integer.valueOf(arg.split(":")[1].trim());
+            }
+        }
+
+        for(int i = 0; i < count; i++){
+            Runnable runnable = () -> {
+                Socket s;
+                try{
+                    int port = ports.getAndIncrement();
+                    ServerSocket serverSocket = new ServerSocket(port);
+                    while(true){
+                        s = serverSocket.accept();
+                        BufferedReader read = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                        Socket clientSocket =  new Socket("localhost", servicePort);
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        out.println(read.readLine());
+                        read.close();
+                        s.close();
+                        out.close();
+                        clientSocket.close();
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            };
+            new Thread(runnable).start();
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,87 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Main {
+
+    private static final ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<>();
+
+    private static final int servicePort = 65000;
+
+    private static final int startPort = 30_000;
+
+    private static final int endPort = 50_000;
+
+
+    private static String getClassPathFromParent() {
+        return System.getProperty("java.class.path", "./*");
+    }
+
+    private static String getJavaCmdFromParent() {
+        return Objects.isNull(System.getProperty("java.home")) ? "java" : String.format("%s%sbin%sjava", System.getProperty("java.home"), File.separator, File.separator);
+    }
+
+    // JVM OPTS -Xmx10g -Xms10g -Xss2048k
+    public static void main(String[] args) throws Exception{
+
+        final String javaCmd = Main.getJavaCmdFromParent();
+        final String classpath = Main.getClassPathFromParent();
+
+        int childProcessesCount = (endPort - startPort) / 4_000;
+
+        System.out.println("Child processes will be spawned (4000 threads each): " + childProcessesCount);
+
+        int lastProcess = (endPort - startPort) % 4_000;
+
+        System.out.println("Last processes thread count: " + lastProcess);
+
+        ArrayList<Process> childs = new ArrayList<>();
+
+        for(int i = 0; i < childProcessesCount; i++){
+            final ProcessBuilder proc = new ProcessBuilder(javaCmd, "-cp", classpath, Child.class.getName(), "servicePort:65000", "startPort:" + (startPort + i * 4000), "count:4000");
+            proc.redirectErrorStream(true);
+            proc.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+            childs.add(proc.start());
+        }
+
+        final ProcessBuilder proc = new ProcessBuilder(javaCmd, "-cp", classpath, Child.class.getName(), "servicePort:65000", "startPort:" + (startPort + childProcessesCount * 4000), "count:" + lastProcess);
+        proc.redirectErrorStream(true);
+        proc.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        childs.add(proc.start());
+
+        new Thread(() -> {
+            Socket s;
+            try{
+                ServerSocket serverSocket = new ServerSocket(servicePort);
+                while(true){
+                    s = serverSocket.accept();
+                    BufferedReader read = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    queue.add(read.readLine());
+                    read.close();
+                    s.close();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }).start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            childs.forEach(Process::destroyForcibly);
+            System.out.println("Destroyed");
+        }));
+
+        while(true){
+            Thread.sleep(50);
+            String str = queue.poll();
+            if(str != null) System.out.println(str);
+        }
+
+    }
+}
